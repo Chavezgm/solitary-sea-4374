849fcda215a105448e92f6151266c4603b98dace
Martin Chavez <martinchavez1203@gmail.com>
2024-04-05 11:03:49 -0600

    final commit

--- spec/features/air_bender_spec.rb
+++ spec/features/air_bender_spec.rb
@@ -13,12 +13,13 @@ RSpec.feature "Viewing Fire Nation Members", type: :feature do
     click_button "Search For Members"
 
     #  redirected to the search page
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_current_path(search_path)
     # Ensure the total number of people who live in the Fire Nation is displayed (close to 100)
     expect(page).to have_content("Total Members: Approximately")
 
     # Ensure a list with the detailed information for the first 25 members of the Fire Nation is displayed
-    
+
+  
   end
 end
