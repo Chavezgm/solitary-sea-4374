c597c6a46724cecc1dfab3e43bf7b54ea9f7e492
Martin Chavez <martinchavez1203@gmail.com>
2024-04-05 10:58:17 -0600

    create air bender facade

---
+++ app/facades/air_bender_facade.rb
@@ -0,0 +1,17 @@
+class AirBenderFacade
+  attr_reader :members
+
+  def initialize(nation, service = AirBenderService.new)
+    @nation = nation
+    @service = service
+    @members = nil
+  end
+
+  def get_nation_members
+    @members ||= begin
+      member_data = @service.get_nation_members(@nation, @name)
+      # require 'pry'; binding.pry
+      @members = member_data.map { |member_info| Member.new(member_info) }
+    end
+  end
+end 
